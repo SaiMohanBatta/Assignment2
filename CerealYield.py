d1ffbdc6cf457e086145c66b35c7c1e60b056e10
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-02 21:34:51 +0000

    Update CerealYield.py
    updated code with numpy,scipy and Statistical methods

--- CerealYield.py
+++ CerealYield.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import describe, pearsonr
 
 def Plot_Line(path_to_file):
     
@@ -29,6 +31,24 @@ def Plot_Line(path_to_file):
     # Display the plot after generating
     plt.show()
 
+    # Statistical Analysis using NumPy and SciPy
+    # Calculate mean and standard deviation
+    mean_yield = np.mean(data.iloc[:, 1:], axis=0)
+    std_yield = np.std(data.iloc[:, 1:], axis=0)
+
+    # Descriptive statistics using SciPy's describe function
+    stats_result = describe(data.iloc[:, 1:], axis=1)
+
+    # Calculate correlation coefficient between the first two countries
+    country1_yield = data.iloc[0, 1:]
+    country2_yield = data.iloc[1, 1:]
+    correlation_coefficient, _ = pearsonr(country1_yield, country2_yield)
+
+    # Print the statistical results
+    print("Mean Cereal Yield: ", mean_yield)
+    print("Standard Deviation of Cereal Yield: ", std_yield)
+    print("Descriptive Statistics: ", stats_result)
+    print("Correlation Coefficient between Country 1 and Country 2: ", correlation_coefficient)
 
 # Calling the function to generate the Line Plot
 path_to_file = 'CerealYield.csv'
