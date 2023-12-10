4fd8bd40cc728f716e6e20f32eef61ab4fcd3b03
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-10 21:11:25 +0000

    Update LifeExp.py
    made changes to the code that it aligns with pep-8 and good coding practices.added good comments for easy understanding

--- LifeExp.py
+++ LifeExp.py
@@ -1,35 +1,85 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import skew, kurtosis
 
-def plot_LifeExp(data):
+
+def read_data(file_name):
+    
+    """
+    Reads a dataframe in World Bank format and returns two dataframes:
+    one with years as columns and one with countries as columns.
+
+    Parameters:
+    - file_name (str): The file path of the CSV file.
+
+    Returns:
+    Tuple[pd.DataFrame, pd.DataFrame]: Two dataframes.
+    """
+    # Read the CSV file
+    df = pd.read_csv(file_name, index_col='Country Name')
+
+    # DataFrame with years as columns
+    y_col = df.copy()
+
+    # DataFrame with countries as columns
+    c_col = df.transpose()
+
+    # Cleaning the transposed dataframe
+    c_col.index = pd.to_numeric(c_col.index, 
+                                errors='coerce')
+    c_col.dropna(inplace=True)
+
+    return y_col, c_col
+
+
+def plot_lifeexp(data):
+    
+    """
+    Plots life expectancy data for multiple countries over the years.
+
+    Parameters:
+    - data (dict): Life expectancy data for different countries.
+
+    Returns:
+    None
+    """
     countries = list(data.keys())
     years = list(data[countries[0]].keys())
-    
-    bar_width = 0.1  # Adjust this value to make bars thinner or thicker
-    
-    fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the figure size as needed
-    
+
+    bar_width = 0.1  # Bar thickness
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
     for i, year in enumerate(years):
         values = [data[country][year] for country in countries]
-        # Offset the bars for each year to avoid overlap
-        ax.bar([x + i * bar_width for x in range(len(countries))], values, width=bar_width, label=year)
+        ax.bar([x + i * bar_width for x in range(len(countries))],
+               values, width=bar_width, label=year)
 
     ax.set_xlabel('Countries')
-    ax.set_ylabel('Life Expectancy(total years')
-    ax.set_title('Life Expetency at Birth')
-    
-    # Center the x-ticks under each group of bars
-    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 for i in range(len(countries))])
+    ax.set_ylabel('Life Expectancy (years)')
+    ax.set_title('Life Expectancy at Birth')
+
+    # Center the x-ticks
+    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 
+                   for i in range(len(countries))])
     ax.set_xticklabels(countries)
-    
-    # Move legend outside the plot area
+
+    # Adjust legend position
     ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.show()
 
-# Read data from CSV
-file_path = 'LifeExp.csv'
-LifeExp_data = pd.read_csv(file_path, index_col='Country Name').to_dict(orient='index')
+    # using statistics methods
+    for country in countries:
+        values = np.array([data[country][year] for year in years])
+        print(f"\nStatistics for {country}:")
+        print(pd.DataFrame(values).describe())
+        print(f"Skewness: {skew(values)}")
+        print(f"Kurtosis: {kurtosis(values)}")
+        
 
-# Plotting the data with a larger figure size and adjusted legend position
-plot_LifeExp(LifeExp_data)
+# using above functions for the data
+file_path = 'LifeExp.csv'
+years_df, countries_df = read_data(file_path)
+plot_lifeexp(years_df.to_dict(orient='index'))
