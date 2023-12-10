30913eece88a8347bb3c2cd12a857188b20007d7
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-10 21:10:33 +0000

    Update HeatMap_China.py
    made changes to the code that it aligns with pep-8 and good coding practices.added good comments for easy understanding

--- HeatMap_China.py
+++ HeatMap_China.py
@@ -1,33 +1,65 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis
 
-def Plot_Heatmap(file_path, indicators):
+
+def plot_heatmap(file_path, indicators):
     
-    # Read data from CSV file into a DataFrame
+    """
+    Generate a correlation heatmap for specified indicators 
+    and display descriptive statistics.
+
+    Parameters:
+    - file_path (str): The path to the CSV file containing the data.
+    - indicators (list): A list of indicators to include in the analysis.
+
+    Returns:
+    None
+    """
+    # Read data from CSV file 
     data = pd.read_csv(file_path)
 
     # Ensure 'Life Expectancy' is in the list of indicators
     if 'Life Exp' not in data.columns:
         raise ValueError("Column 'Life Exp' not found in the DataFrame.")
 
-    # Extract relevant columns for correlation analysis
-    indicators_data = data[indicators + ['Life Exp']]  # Include 'Life Expectancy' in the list
+    # Extract relevant columns for analysis
+    ind_data = data[indicators + ['Life Exp']]
 
     # Calculate correlation matrix
-    correlation_matrix = indicators_data.corr()
+    corr_mat = ind_data.corr()
 
     # Plot the heatmap
     plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
+    sns.heatmap(corr_mat, annot=True, cmap='coolwarm', 
+		fmt=".2f", linewidths=.5)
     plt.title('Correlation Heatmap for China')
     plt.show()
 
-# Replace 'your_data.csv' with the actual filename or path to your CSV file
+    # Describe function using Numpy
+    print("\nDescriptive Statistics:")
+    print(ind_data.describe())
+
+    # Calculate skewness and kurtosis using Scipy
+    print("\nSkewness:")
+    for i in indicators:
+        skewness = skew(ind_data[i])
+        print(f"{i}: {skewness:.4f}")
+
+    print("\nKurtosis:")
+    for i in indicators:
+        kurt = kurtosis(ind_data[i])
+        print(f"{ind_data}: {kurt:.4f}")
+
+#filename of CSV file
 file_path = 'ChinaData.csv'
 
-# Specify the indicators of interest
-selected_indicators = ['Urbanization', 'Cereal Yield', 'Agriculture Land', 'Fertilizer Consumption']
+# Specifying the indicators
+sel_ind = [
+    'Urbanization', 'Cereal Yield', 'Agriculture Land', 
+    'Fertilizer Consumption', 'GreenHouse Gas'
+]
 
-# Call the function to generate the heatmap
-Plot_Heatmap(file_path, selected_indicators)
+# Calling the function
+plot_heatmap(file_path, sel_ind)
