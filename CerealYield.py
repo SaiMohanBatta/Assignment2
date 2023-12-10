c831ce1ce6b6fcbb425ac0296df719eb139e35e1
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-10 21:09:49 +0000

    Update CerealYield.py
    made changes to the code that it aligns with pep-8 and good coding practices.added good comments for easy understanding

--- CerealYield.py
+++ CerealYield.py
@@ -1,55 +1,78 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import describe, pearsonr
+from scipy.stats import kurtosis, skew
 
-def Plot_Line(path_to_file):
-    
-    # Load the dataset from the CSV file
-    data = pd.read_csv(path_to_file)
 
-    # Getting the data of country codes and years
-    # Fetched the country codes from the CSV
+def read_data(file_path):
+    """
+    Reads data from a CSV file and returns two dataframes: one with years as
+    columns and one with countries as columns.
+
+    Parameters:
+    - file_path (str): Path to the CSV file.
+
+    Returns:
+    Tuple[pd.DataFrame, pd.DataFrame]: DataFrames with different orientations.
+    """
+    # Load the dataset
+    data = pd.read_csv(file_path)
+
+    # Transpose the data 
+    t_data = data.T
+
+    # Cleaning the transposed dataframe
+    t_data.columns = t_data.iloc[0]
+    t_data = t_data.drop(t_data.index[0])
+
+    return data, t_data
+
+
+def plot_line(data):
+    """
+    Generate a line plot representing cereal yield from 
+    1970 to 2020 for different
+    countries. Additionally, prints statistical descriptions of the data.
+
+    Parameters:
+    - data (pd.DataFrame): DataFrame containing the dataset.
+
+    Returns:
+    None
+    """
+    # get country codes and years
     country_codes = data['Country Name']
-    # The columns of years start from the 3rd column.
     years = data.columns[1:]
 
-    # Create a line plot with multiple lines based on the number of countries
+    # Create a line plot
     plt.figure(figsize=(10, 6))
     for idx in range(len(country_codes)):
-        # The data of years starts from column 3.
         plt.plot(years, data.iloc[idx, 1:], label=country_codes[idx])
 
-    # Adding plot title and axis labels to append to the graphs.
+    # Add plot axis names
     plt.title('Cereal Yield from 1970 to 2020')
     plt.xlabel('Years')
     plt.ylabel('Cereal yield (kg per hectare)')
-
-    # Adding legend to the map countrywide
     plt.legend()
-
-    # Display the plot after generating
     plt.show()
 
-    # Statistical Analysis using NumPy and SciPy
-    # Calculate mean and standard deviation
+    # Using statistic methods
     mean_yield = np.mean(data.iloc[:, 1:], axis=0)
     std_yield = np.std(data.iloc[:, 1:], axis=0)
+    skewness_yield = skew(data.iloc[:, 1:], axis=1)
+    kurtosis_yield = kurtosis(data.iloc[:, 1:], axis=1)
 
-    # Descriptive statistics using SciPy's describe function
-    stats_result = describe(data.iloc[:, 1:], axis=1)
-
-    # Calculate correlation coefficient between the first two countries
-    country1_yield = data.iloc[0, 1:]
-    country2_yield = data.iloc[1, 1:]
-    correlation_coefficient, _ = pearsonr(country1_yield, country2_yield)
-
-    # Print the statistical results
+    # using statistical methods
+    stats = data.iloc[:, 1:].describe()
+    print("\nDescriptive Statistics:\n", stats)
     print("Mean Cereal Yield: ", mean_yield)
-    print("Standard Deviation of Cereal Yield: ", std_yield)
-    print("Descriptive Statistics: ", stats_result)
-    print("Correlation Coefficient between Country 1 and Country 2: ", correlation_coefficient)
+    print("Standard Deviation: ", std_yield)
+    print("Skewness: ", skewness_yield)
+    print("Kurtosis: ", kurtosis_yield)
 
-# Calling the function to generate the Line Plot
-path_to_file = 'CerealYield.csv'
-Plot_Line(path_to_file)
+
+
+# calling functions to generate line plot
+file_path = 'CerealYield.csv'
+data, t_data = read_data(file_path)
+plot_line(data)
