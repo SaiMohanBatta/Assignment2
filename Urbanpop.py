10c698ac74b01a82a1bbff1d40c1f6fe0d429da9
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-10 21:11:49 +0000

    Update Urbanpop.py
    made changes to the code that it aligns with pep-8 and good coding practices.added good comments for easy understanding

--- Urbanpop.py
+++ Urbanpop.py
@@ -1,35 +1,71 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import skew, kurtosis
 
-def plot_Urbanpop(data):
+
+def read_data(file_path):
+    
+    """
+    Read urbanization data from a CSV file and return two dataframes.
+
+    Parameters:
+    - file_path (str): Path to the CSV file.
+
+    Returns:
+    - DataFrame: With years as columns.
+    - DataFrame: With countries as columns.
+    """
+    # Read data from CSV
+    df = pd.read_csv(file_path, index_col='Country Name')
+
+    # Transpose the dataframe
+    df_transposed = df.transpose()
+    
+    return df, df_transposed
+
+
+def plot_urbanpop(data):
+    
+    """
+    Plot urbanization rate over years for various countries.
+
+    Parameters:
+    - data (dict): Urbanization rate data for countries and years.
+    """
     countries = list(data.keys())
     years = list(data[countries[0]].keys())
-    
-    bar_width = 0.1  # Adjust this value to make bars thinner or thicker
-    
-    fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the figure size as needed
-    
+    bar_width = 0.1
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
     for i, year in enumerate(years):
         values = [data[country][year] for country in countries]
-        # Offset the bars for each year to avoid overlap
-        ax.bar([x + i * bar_width for x in range(len(countries))], values, width=bar_width, label=year)
+        x_positions = [x + i * bar_width for x in range(len(countries))]
+        ax.bar(x_positions, values, width=bar_width, label=year)
 
     ax.set_xlabel('Countries')
     ax.set_ylabel('Urbanization rate')
-    ax.set_title('Urbanization Rate Over the Years for Different Countries')
-    
-    # Center the x-ticks under each group of bars
-    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 for i in range(len(countries))])
+    ax.set_title('Urbanization Rate Over Years for Countries')
+    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 
+                   for i in range(len(countries))])
     ax.set_xticklabels(countries)
-    
-    # Move legend outside the plot area
     ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.show()
 
-# Read data from CSV
-file_path = 'UrbanPop.csv'
-Urbanpop_data = pd.read_csv(file_path, index_col='Country Name').to_dict(orient='index')
+    Val_arr = np.array([data[country][year] 
+                        for year in years for country in countries])
+    print("Descriptive Statistics:")
+    print(pd.DataFrame(Val_arr).describe())
+    print("\nSkewness:", skew(Val_arr))
+    print("Kurtosis:", kurtosis(Val_arr))
 
-# Plotting the data with a larger figure size and adjusted legend position
-plot_Urbanpop(Urbanpop_data)
+
+# plotting using Data
+file_path = 'UrbanPop.csv'
+df, df_transposed = read_data(file_path)
+
+# Convert DataFrame to dictionary for plot function
+urbanpop_data = df.to_dict(orient='index')
+plot_urbanpop(urbanpop_data)
