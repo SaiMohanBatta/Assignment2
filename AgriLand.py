a634de7930d2773d803abbfbf3be35155b8524d0
SaiMohanBatta <149528797+SaiMohanBatta@users.noreply.github.com>
2023-12-10 21:09:20 +0000

    Update AgriLand.py
    made changes to the code that it aligns with pep-8 and good coding practices.added good comments for easy understanding

--- AgriLand.py
+++ AgriLand.py
@@ -1,46 +1,86 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import kurtosis, skew
 
-def Plot_Line(path_to_file):
-    # Load the dataset from the CSV file
-    data = pd.read_csv(path_to_file)
 
-    # Getting the data of country codes and years
-    # Fetched the country codes from the CSV
-    country_codes = data['Country Name']
-    # The columns of years start from the 3rd column.
-    years = data.columns[1:]
+def read_data(filename):
+    
+    """
+    Reads a dataframe in Worldbank format and returns two dataframes:
+    one with years as columns and one with countries as columns.
 
-    # Describe the dataset to get summary statistics
+    Parameters:
+    - filename (str): Path to the CSV file containing the dataset.
+
+    Returns:
+    - Tuple of two dataframes
+    """
+    # Load dataset
+    data = pd.read_csv(filename, index_col=0)
+
+    # Transpose the data
+    df_years = data.transpose()
+    df_countries = data
+
+    # Clean dataframes
+    df_years = df_years.apply(pd.to_numeric, errors='coerce')
+    df_countries = df_countries.apply(pd.to_numeric, errors='coerce',
+                                      axis=1)
+
+    return df_years, df_countries
+
+
+def plot_line(path_to_file):
+    """
+    Generate a line plot showing agricultural land area trends
+    from 1970 to 2020 for multiple countries.
+
+    Parameters:
+    - path_to_file (str): Path to the CSV file containing the dataset.
+
+    Returns:
+    None
+    """
+    # Load dataset
+    df_y, df_c = read_data(path_to_file)
+
+    # Get country codes and years
+    country_codes = df_c.index
+    years = df_y.index
+
+    # Describe the dataset for summary statistics
     print("Summary Statistics:")
-    print(data.describe())
+    print(df_c.describe())
 
-    # Create a line plot with multiple lines based on the number of countries
+    # Create line plot
     plt.figure(figsize=(10, 6))
-    for idx in range(len(country_codes)):
-        # The data of years starts from column 3.
-        plt.plot(years, data.iloc[idx, 1:], label=country_codes[idx])
+    for country_code in country_codes:
+        plt.plot(years, df_c.loc[country_code], label=country_code)
 
-    # Adding plot title and axis labels to append to the graphs.
+    # Add plot title and axis labels
     plt.title('Agricultural land area from 1970 to 2020')
     plt.xlabel('Years')
     plt.ylabel('Agricultural land (% of land area)')
 
-    # Adding legend outside the plot
+    # Add legend outside plot
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
-    # Display the plot after generating
+    # Display plot
     plt.show()
 
-    # Additional statistical information
-    print("\nAdditional Statistics:")
+    # using statistical methods
+    print(" Statistics:")
     for country_code in country_codes:
-        country_data = data[data['Country Name'] == country_code].iloc[:, 1:]
+        country_data = df_c.loc[country_code]
         print(f"\nStatistics for {country_code}:")
-        print(f"Mean: {country_data.mean(axis=1).values[0]}")
-        print(f"Median: {country_data.median(axis=1).values[0]}")
-        print(f"Standard Deviation: {country_data.std(axis=1).values[0]}")
+        print(f"Mean: {np.mean(country_data)}")
+        print(f"Median: {np.median(country_data)}")
+        print(f"Standard Deviation: {np.std(country_data)}")
+        print(f"Kurtosis: {kurtosis(country_data)}")
+        print(f"Skewness: {skew(country_data)}")
 
-# Calling the function to generate the Line Plot
+
+# Calling function to generate Line Plot
 path_to_file = 'AgriLand.csv'
-Plot_Line(path_to_file)
+plot_line(path_to_file)
